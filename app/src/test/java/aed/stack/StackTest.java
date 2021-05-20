3f3b9c1d774a2f6370e9db7440747cd8dbe8d44f
Daniel Rocha <rochadaniel15@gmail.com>
2021-05-20 11:15:23 -0300

    add: Adicionado testes para a Stack

---
+++ app/src/test/java/aed/stack/StackTest.java
@@ -0,0 +1,51 @@
+package aed.stack;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import aed.stack.Stack;
+
+public class StackTest {
+  Stack<String> stack;
+
+  @Before
+  public void initialize() {
+    stack = new Stack<>();
+    stack.push("Daniel");
+    stack.push("Rocha");
+    stack.push("Silva");
+  }
+
+  @Test
+  public void verifyIfTopStackElementIsCorrect() {
+    var element = stack.peek();
+    var expect = "Silva";
+
+    assertEquals(element, expect);
+  }
+
+  @Test
+  public void verifyIfElementIsRemovedWhenIsGetTheLastOfTheStack() {
+    var expectLength = stack.length() - 1;
+    var expect = "Silva";
+    var element = stack.pop();
+
+    assertEquals(element, expect);
+    assertEquals(stack.length(), expectLength);
+  }
+
+  @Test
+  public void shouldCloneStack() {
+    var stackClone = stack.clone();
+    
+    assertEquals(stackClone.length(), stack.length());
+
+    while(!stackClone.isEmpty()) {
+      var elementA = stackClone.pop();
+      var elementB = stack.pop();
+      assertEquals(elementA, elementB);
+    }
+  }
+}
