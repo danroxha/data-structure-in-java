feeae884720502234abb8d4520d3dc25f71ae8a7
Daniel Rocha <rochadaniel15@gmail.com>
2021-05-23 22:15:08 -0300

    refactor: Removido codigo desnecessário

--- app/src/test/java/aed/tree/TreeTest.java
+++ app/src/test/java/aed/tree/TreeTest.java
@@ -59,9 +59,5 @@ public class TreeTest {
     var list = tree.toLinkedList();
 
     assertEquals(list.length(), tree.size());
-
-    for(int i = 0; i < list.length(); i++) {
-      System.out.println(list.get(i));
-    }
   }
 }
