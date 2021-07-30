208ba4258b4500118e939a78578f25aee302a5f4
Daniel Rocha <rochadaniel15@gmail.com>
2021-07-29 22:45:21 -0300

    add: Adicinado sort: cocktail

--- app/src/main/java/aed/sort/Sort.java
+++ app/src/main/java/aed/sort/Sort.java
@@ -8,10 +8,46 @@ public class Sort {
   public static <T>void quickSort(T[] collection, Comparator<? super T> comparator) {
     sort(collection, 0, collection.length - 1, comparator);
   }
+          
+  public static <T>void cocktail(T[] collection, Comparator<? super T> comparator) {
+    
+    var low = 0; 
+    var high = collection.length - 1;
+    
+    while (low < high) {
+      
+      
+      for (int i = low; i < high; i = i + 1) {
+        var diff = comparator.compare(collection[i], collection[i + 1]);
+        if (diff > 0) {
+          var swap = collection[i];
+          collection[i] = collection[i + 1];
+          collection[i + 1] = swap;
+        }
+      }
+      
+      high = high - 1;
+        
+      for (int i = high; i > low; i = i - 1) {
+        
+        var diff = comparator.compare(collection[i], collection[i - 1]);
 
+        if (diff < 0) {
+          var swap = collection[i];
+          collection[i] = collection[i - 1];
+          collection[i - 1] = swap;
+        }
+      }
+
+      low = low + 1;
+    }
+  }
+
+
+  /**QuickSort start*/
 	private static <T>int partition(T collection[], int low, int high, Comparator<? super T> comparator) {
 		var pivot = collection[high];
-		int i = (low - 1);
+		var i = (low - 1);
 		
     for (var j = low; j < high; j++) {
       var diff = comparator.compare(collection[j], pivot);
@@ -38,4 +74,5 @@ public class Sort {
 		sort(collection, low, middle - 1, comparator);
 		sort(collection, middle + 1, high, comparator);
 	}
+  /**QuickSort end*/
 }
--- app/src/test/java/aed/sort/SortTest.java
+++ app/src/test/java/aed/sort/SortTest.java
@@ -17,17 +17,13 @@ public class SortTest {
   String[] listOrderString;
   String[] listDesorderString;
 
-
   @Before   
   public void setUp() {
-    
     listOrderInteger = new Integer[]{0, 1, 2, 3, 4, 5};
     listDesorderInteger = new Integer[]{5, 0, 3, 4, 2, 1};
     
     listOrderString = new String[]{"daniel", "rocha", "silva"};
-    listDesorderString = new String[]{"silva", "daniel", "rocha"};
-
-     
+    listDesorderString = new String[]{"silva", "daniel", "rocha"};     
   }
 
   @Test
@@ -39,6 +35,18 @@ public class SortTest {
   @Test
   public void shouldSortStringListWithQuickSort() {
     Sort.<String>quickSort(listDesorderString, (a, b) -> a.compareTo(b));
+    assertArrayEquals(listOrderString, listDesorderString);
+  }
+  
+  @Test
+  public void shouldSortIntegerListWithCocktail() {
+    Sort.<Integer>cocktail(listDesorderInteger, (a, b) -> a - b);  
+    assertArrayEquals(listOrderInteger, listDesorderInteger);
+  }
+
+  @Test
+  public void shouldSortStringListWithCocktail() {
+    Sort.<String>cocktail(listDesorderString, (a, b) -> a.compareTo(b));
     assertArrayEquals(listOrderString, listDesorderString);
   }
 }
